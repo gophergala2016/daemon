571e2860cc68df812fac87757a80025222eb0143
Bryan Allred <bryan.allred@gmail.com>
2016-01-22 22:21:49 -0500

    Basic framework for cmd/run

--- cmd/run.go
+++ cmd/run.go
@@ -1,10 +1,26 @@
 package cmd
 
-import "github.com/codegangsta/cli"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/codegangsta/cli"
+)
+
+const (
+	// The interval between scouring the interwebs
+	interval = 15 * time.Minute
+)
 
 var (
-	executable string
-	feeds      string
+	// EnvironmentVariables is a key/value pairing storing default values.
+	EnvironmentVariables = map[string]string{
+		"DAEMON_STORY_DIR": "./stories",
+		"DAEMON_FEEDS":     "./feeds.txt",
+	}
+
+	// CommandRun is the command to execute the main daemon processes.
 	CommandRun = cli.Command{
 		Name:      "run",
 		ShortName: "",
@@ -14,6 +30,62 @@ var (
 	}
 )
 
+// Run the main daemon processes.
 func run(context *cli.Context) {
-	// Stub
+	// At an interval scour the interwebs for stories to trigger events
+	doEvery(interval, func() {
+		log.Println("Scanning")
+	})
+}
+
+// Repeat a piece of logic on a scheduled interval.
+func doEvery(d time.Duration, f func()) {
+	for _ = range time.Tick(d) {
+		f()
+	}
+}
+
+// Initialize the environment variables.
+func initEnvironment() {
+	log.Println("Initializing the Daemon")
+
+	// Setup the environment variables with defaults if they are missing
+	for key, value := range EnvironmentVariables {
+		env := os.Getenv(key)
+		if env == "" {
+			os.Setenv(key, value)
+		}
+	}
+}
+
+// Find the file listing a feed per line to crawl.
+// If the file does not exist then attempt to create it.
+func feedsPath() (string, error) {
+	path := os.Getenv("DAEMON_FEEDS")
+
+	// Check for feeds directory
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		_, err = os.Create(path)
+		if err != nil {
+			return EnvironmentVariables["DAEMON_FEEDS"], err
+		}
+	}
+
+	return path, nil
+}
+
+// Find the directory where all of the stories will be stored.
+// If the directory does not exist then attempt to create it.
+func storyDirectory() (string, error) {
+	path := os.Getenv("DAEMON_STORY_DIR")
+
+	// Check for story directory
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.MkdirAll(path, 0755)
+		if err != nil {
+			return EnvironmentVariables["DAEMON_STORY_DIR"], err
+		}
+	}
+
+	return path, nil
 }
