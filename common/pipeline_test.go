272a3a7bd59a91bac66602310aebd089f534e79b
Bryan Allred <bryan.allred@gmail.com>
2016-01-24 09:21:20 -0500

    Unit testing for pipeline

--- common/pipeline_test.go
+++ common/pipeline_test.go
@@ -1,1 +1,41 @@
 package common
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestPipelineRunWithOneProgram(t *testing.T) {
+	expect := "blah"
+	pipeline := Pipeline{
+		exec.Command("echo", expect),
+	}
+
+	reader := bytes.NewReader([]byte("test input"))
+	output, err := pipeline.Run(reader)
+	if err != nil {
+		t.Error(err)
+	}
+	if strings.TrimSpace(string(output)) != expect {
+		t.Errorf("Expecting %q but was %q\n", expect, output)
+	}
+}
+
+func TestPipelineRunWithMultiplePrograms(t *testing.T) {
+	expect := "8"
+	pipeline := Pipeline{
+		exec.Command("echo", "8\n2\n3"),
+		exec.Command("grep", "8"),
+	}
+
+	reader := bytes.NewReader([]byte("test input"))
+	output, err := pipeline.Run(reader)
+	if err != nil {
+		t.Error(err)
+	}
+	if strings.TrimSpace(string(output)) != expect {
+		t.Errorf("Expecting %q but was %q\n", expect, output)
+	}
+}
